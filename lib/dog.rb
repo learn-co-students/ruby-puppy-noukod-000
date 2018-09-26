e3c14f8e7003942ac95086f94ac91ffa3158aee3
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-26 18:53:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+#Write your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+end
