0ed986cb82283278542de1fbbcce3be5d70c2b8d
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-23 19:00:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(puppy_name)
+    @name = puppy_name
+    @@all << self
+
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    
+    @@all.each do |d_name|
+      puts d_name.name
+    end
+
+  end
+
+end
